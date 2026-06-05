789c9b128827e3b3a7d5c146a46cc9ef8c666c24
Naqeeb <naqeebonline@gmail.com>
2026-06-05 10:52:08 +0500

    files are deleted

--- Modules/Settings/Config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => 'Settings',
-    'app_id' => 1,
-    'active_layout' => 'app_screen_frest',
-    'company_title' => 'Organization',
-    'section_title' => 'Unit'
-];
